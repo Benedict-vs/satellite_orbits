244b4ed43afac181cb73a775afffadc03df9a71e
Benedict von Schubert <benedictvonschubert@gmail.com>
2025-12-11 11:24:39 +0100

    v1

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 
 Goal (v0): Plot a single satellite orbit around earth in 3D using python
-Goal (v1): add current satellite position from online database
+Goal (v1): add earth surface with correct orientation and change background color
+- earth rotation
+- earth atmosphere?
+- zoom function?
+Goal (v2): turn line into dotted line and add moving satellite
+Gaol (v3): 
+
+
+Goal (v): add current satellite position from online database
 Goal
 
 Goal(...): Have database of satellites to choose from to plot
--- satellite_orbits/orbit_plot.py
+++ satellite_orbits/orbit_plot.py
@@ -3,22 +3,39 @@ import pyvista as pv
 from astropy import constants as const
 from astropy import units as u
 
+# Earth data
+earth = pv.examples.planets.load_earth(radius=6378.1)
+earth_texture = pv.examples.load_globe_texture()
+
+# Background data
+cubemap = pv.examples.download_cubemap_space_4k()
+
+# Light (sun)
+light = pv.Light()
+light.set_direction_angle(30, 160)
+
+# Orbit data
 r_earth = const.R_earth.to(u.km).value   # Earth's radius in meters
 altitude_satellite = 500  # km
 r_orbit = r_earth + altitude_satellite
-
-
-# Earth data
-earth_sphere = pv.Sphere(radius=r_earth, theta_resolution=100, phi_resolution=100)
-
-# Orbit data
 theta = np.linspace(0, 2*np.pi, 360)
 points = np.column_stack((r_orbit * np.cos(theta),
                           r_orbit * np.sin(theta),
                           np.zeros_like(theta)))
 orbit_line = pv.Spline(points, 360)
 
+
+# --- Prepare plotter ---
 plotter = pv.Plotter()
-plotter.add_mesh(earth_sphere, smooth_shading=True)
+plotter = pv.Plotter(lighting='none')
+_ = plotter.add_actor(cubemap.to_skybox())
+plotter.set_environment_texture(cubemap, is_srgb=True)
+plotter.add_light(light)
+earth_actor = plotter.add_mesh(earth, texture=earth_texture, smooth_shading=True)
+earth_actor.rotate_x(23.5)
 plotter.add_mesh(orbit_line, line_width=4)
+
+# --- earth specific settings ---
+  
+
 plotter.show()
